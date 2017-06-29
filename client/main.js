c808f8176fca0ad6b21bd02908ef5bf0f78e4439
Gabriel Meyer <meyergabriel@live.de>
2017-06-29 20:00:41 +0200

    First lookout for the battlefield

--- client/main.js
+++ client/main.js
@@ -1,17 +1,18 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react'
 
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
-  this.counter = new ReactiveVar(0);
+  // this.counter = new ReactiveVar(0);
+
 });
 
+
 Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+
 });
 
 Template.hello.events({
