a8bd3153b094c76a7264a0ce851a8182d1497518
GabrielInTheWorld <meyergabriel@live.de>
2017-07-20 14:47:13 +0200

    initial commit

---
+++ client/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {Meteor} from 'meteor/meteor'
+
+
+import App from '../imports/UI/App.jsx'
+
+Meteor.startup(() => {
+    render(<App />, document.getElementById("render-target"))
+})
---
+++ imports/UI/Chat.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import * as bootstrap from 'react-bootstrap'
+
+/**
+ * bootstrap components
+ */
+const Button = bootstrap.Button
+
+export default class Chat extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
